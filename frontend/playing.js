e92bf83fbb1c9e921064b312a0783d8e22939cfe
vospader <spader89d@yahoo.com>
2017-10-07 21:04:13 -0700

    Update playing.js
    added map tiles at random numbers - this needs to be re-worked for new players only

--- frontend/playing.js
+++ frontend/playing.js
@@ -11,27 +11,67 @@ preload: function(){
 
 		this.load.image('titleScreen', 'assets/titleScreen.png');
 		this.load.image("button", "assets/button.png")
-		game.world.setBounds(0,0,2120,1300);
-		
+		//TheMoon.playingConfig(this.game);
 },
 create: function(){
-
+/*
+*This is the base state that will be active while the player is playing. 
+*
+**************functions****************
+*             drawMap()
+*             drawMenu()
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
 	//this object's background = add new sprite to total game(x,y,'name of object that is sprite')
+function drawMap() {
+var tile1, tile2;
+for( y = 0; y <= 1300; y++){
+	for( x = 0; x <= 2100; x++){
+			
+		tile1 = game.rnd.integerInRange(1,3);
+		
+		if(tile1 == 1){
+			game.add.sprite(x,y,'tileHills');
+		}else if(tile1 == 2){
+			game.add.sprite(x,y,'tileWater');
+		}else if(tile1 == 3){
+			game.add.sprite(x,y,'tileGrasslands');
+		}
+x = x +99;
+
+}
+y = y +99;
+}
+
+}
+drawMap();
 this.background = "#00000";
 
-game.add.sprite(0,0,'tileHills');
-game.add.sprite(100,100,'tileHills');
-game.add.sprite(200,200,'tileHills');
-game.add.sprite(1920,1200,'tileHills');
+
 var city;
 city = game.add.button(0, 0, 'button', actionOnClick, this);
 city.scale.setTo(3,0.6);
 
 city.fixedToCamera = true;
 
+game.global.score = 1;
 
 
 },
+
+
+
 update: function(){
 	 if (cursors.up.isDown)
         {
@@ -64,4 +104,6 @@ render: function(){
 
 
 }
+
+
 };
