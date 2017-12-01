1f7afd70477a88f804f7aff035ef9f3214c64dfc
VexyCats <spader89d@yahoo.com>
2017-12-01 14:40:34 -0800

    updated to right file

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,23 +1,39 @@
-var game,
-Boot = function(cgame){
-	this.game = game = cgame;
-};
 
-Boot.prototype = {
+
+/* Boot.js 
+* First state to load
+* var TheMoon is the object for the app
+*	loads assets
+*
+*
+*
+*
+*/
+
+
+
+var TheMoon = TheMoon || {
+
+};
+	
+ TheMoon.Boot = function(game){};
+
+TheMoon.Boot.prototype = {
 
 
 
 	preload:function(){
 
-
+			
 								console.log("boot init");
 this.load.image('preloaderBar', 'assets/preloader.png');
+game.load.image('homeBase','assets/tileHomeBase.jpg');
 
 
 	},
 
 	create:function(){
-
+		
 //this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 game.world.setBounds(0,0,2120,1300);
 
@@ -25,21 +41,21 @@ this.scale.minWidth = 1068;
 this.scale.minHeight = 720;
 this.scale.pageAlignHorizontally = true;
 this.scale.pageAlignVertically = true;
-var cursors = game.input.keyboard.createCursorKeys();
+cursors = game.input.keyboard.createCursorKeys();
 this.input.maxPointers = 1;
 			this.stage.disableVisibilityChange = true;
 			this.state.start('Preloader');
-
+			
 	},
 
 	update:function(){
 
-
-
+   
+		
 	}
 
 
 
 }
 
-export default Boot;
+
