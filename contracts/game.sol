31ef380b5bd6fae4fc85de2ff06710d7eed392bb
VexyCats <spader89d@yahoo.com>
2017-12-01 19:32:45 -0800

    added a few functions
    save() and createAccount() - Not done, but just laid it out

--- contracts/game.sol
+++ contracts/game.sol
@@ -8,7 +8,7 @@ contract Game {
         require(msg.sender == owner);
         _;
     }
-    
+    uint256 totalPlayers;
     struct Planet {
         bytes32 name;
         uint iron;
@@ -18,9 +18,40 @@ contract Game {
         uint spacegoo;
     }
     mapping(address => Planet) public planets;
-
+    struct player{
+        uint256 playerID;
+        uint[20] inventory;
+        bool CoolDown;
+        uint lastBlockTime;
+        uint[10] resources;
+    }
+    mapping (address => player) public listOfPlayers;
+    
     function Game() {
+    
+    totalPlayers = 0;
         
+    }
+    /* Needs to take player address and create a new player
+    *   gives starter amount
+    *
+    */
+      function createAccount(){
+     
+    
+     listOfPlayers[msg.sender].playerID = totalPlayers++;
+     
+     listOfPlayers[msg.sender].goldAmount  = 10;
+        }
+        
+        /* Needs to save the player data
+        *   Will be called from the phaser game after so many minutes has cooled down between requests
+        *   needs to save: map tiles, resource amount, inventory amount, player stats, probably more
+        *
+        */
+    function save(){
+    
+    
     }
     
     function spawn(bytes32 planetName) payable {
