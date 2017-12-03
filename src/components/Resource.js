818f0eca020fc160890f247e6ec334878bc6dd2f
Adibas03 <adibas03@ymail.com>
2017-12-03 21:54:52 +0100

    add resource component

---
+++ src/components/Resource.js
@@ -0,0 +1,28 @@
+var game,
+Resource = function(cgame,config){
+  this.game = cgame;
+  config = config || {};
+  var errors=[];
+
+  if(typeof cgame == 'undefined'){
+    console.error('Game required');
+    return false;
+  }
+
+  for(var i in this.requiredConfig){
+    if( typeof config[this.requiredConfig[i]] == 'undefined' || config[this.requiredConfig[i]] == '' )
+      errors.push('Required config: '+ this.requiredConfig[i]);
+  }
+
+  if(errors.length > 0){
+    console.error(errors.join(', ') );
+    return false
+  }
+}
+
+Resource.prototype = {
+    buildConfirmations:{},
+    spawnConfirmations:{}
+}
+
+export default Resource;
