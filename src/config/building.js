025895db0bd00641d609d3c2ce6ed3b32db2a940
Adibas03 <adibas03@ymail.com>
2017-12-05 08:16:33 +0100

    Add building config

---
+++ src/config/building.js
@@ -0,0 +1,22 @@
+var BuldingTypes = {
+      wood:{
+        sprite:'house'
+      },
+      soil:{},
+      water:{},
+      sand:{
+        sprite:'house'
+      }
+    }
+
+var BuildingFonts = {
+  infoBox:{
+    title:{font:'bold 7pt Arial',align:'center'},
+    label:{font:'bold 5pt Arial',align:'left'},
+    body:{font:'normal 5pt Arial',wordWrap:true,wordWrapWidth:200},
+    caption:{font:'normal 3pt Arial',wordWrap:true,wordWrapWidth:200}
+  }
+}
+
+
+export {BuldingTypes,BuildingFonts};
