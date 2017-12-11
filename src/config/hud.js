e9499f8e7403e1cdca5f2ddf8d7b5b24d9688fec
Adibas03 <adibas03@ymail.com>
2017-12-11 02:57:42 +0100

    update hud buttons

--- src/config/hud.js
+++ src/config/hud.js
@@ -5,18 +5,21 @@ var hudConfig = {
   opacity: 0.9,
   fadeOpacity: 0.5,
   saveButton:{
-    x:645,
+    x:640,
     y:465,
     sprite: 'hud_buttons',
-    frame:4,
+    frame:1,
+    //frame:4,
     content:'Save',
     anchor:0.5
   },
   buildButton:{
-    x:505,
+    x:485,
+    //x:505,
     y:465,
     sprite: 'hud_buttons',
-    frame:2,
+    frame:0,
+    //frame:2,
     content:'Save',
     anchor:0.5
   }
