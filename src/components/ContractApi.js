a1a0e3260876fdf528f41c8719d799093574b68b
Adibas03 <adibas03@ymail.com>
2017-12-12 22:10:24 +0100

    update contract api

--- src/components/ContractApi.js
+++ src/components/ContractApi.js
@@ -1,6 +1,6 @@
-//import 'web3/index';
-//import Abi from '../config/abi';
-//import Address from '../config/contract';
+import 'web3/index';
+import Abi from '../config/abi';
+import {Address} from '../config/contract';
 
 var Contract = function(){
   window? '' : window = {};
@@ -20,14 +20,57 @@ var Contract = function(){
     return;
   }
 
-  if(!web3.isConnected){
+  if(!web3.isConnected() ){
     console.error('Web3 not connected to suitable provider');
+    return;
   }
 
+  this.web3 = web3;
+
+  if(typeof web3.eth.accounts != 'object' ||  web3.eth.accounts.length < 1){
+    console.error('No account found, or accounts locked');
+    return;
+  }
+
+  web3.eth.defaultAccount = web3.eth.accounts[0];
+  this.accounts = web3.eth.accounts;
   console.log(web3)
+
+  this.contract = web3.eth.contract(Abi).at(Address);
 }
 
 Contract.prototype = {
+
+  fetchPlayer: function(){
+
+  },
+  createPlayer: function(){
+    const name = prompt('Kindly set your Screen name');
+
+    this.contract.createAccount(name,function(e,r){
+      if(e)
+        console.error(e);
+      console.log('player created',r);
+    })
+  },
+  saveGame: function(buildingData,upgradeData){
+
+    this.contract.save(buildingData,upgradeData,function(e,r){
+      if(e)
+        console.error(e);
+      console.log('player created',r);
+    })
+  },
+  harvestResources: function(buildingsToHarvest){
+
+    this.contract.harvestResources(buildingsToHarvest,function(e,r){
+      if(e)
+        console.error(e);
+      console.log('player created',r);
+    })
+  }
+
+
 
 
 
