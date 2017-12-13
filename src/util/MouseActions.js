88908446872bf1280600e452cc7277af4bc4f3a3
Adibas03 <adibas03@ymail.com>
2017-12-13 01:51:31 +0100

    add more MouseActions

--- src/util/MouseActions.js
+++ src/util/MouseActions.js
@@ -1,11 +1,23 @@
 var Actions = {
   addHover: function(button){
     var scale = {x:button.scale.x,y:button.scale.y}
+    button.input.useHandCursor = true;
     button.events.onInputOver.add(function(btn){
       btn.scale.x = scale.x*1.06,
       btn.scale.y = scale.y*1.06;
     })
     button.events.onInputOut.add(function(btn){
+      btn.scale.x = scale.x,
+      btn.scale.y = scale.y;
+    })
+  },
+  addClicked: function(button){
+    var scale = {x:button.scale.x,y:button.scale.y}
+    button.events.onInputDown.add(function(btn){
+      btn.scale.x = scale.x*0.78,
+      btn.scale.y = scale.y*0.78;
+    })
+    button.events.onInputUp.add(function(btn){
       btn.scale.x = scale.x,
       btn.scale.y = scale.y;
     })
